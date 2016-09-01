244b2e67e22bb9dcba68ad63a90b8be00702040b
Gaurav Tamba <gaurav.tamba@gmail.com>
2016-09-01 22:26:13 +0530

    Update init.py

--- init.py
+++ init.py
@@ -10,5 +10,3 @@
 # THERE'S AN ALIEN INVADER SOMEWHERE IN THE CODEBASE AND OUR SYSTEMS CAN'T TERMINATE IT
 # BECAUSE ITS TOO ADORABLE, PLEASE COMMENT HIM OR THIS PROGRAM SHALL NEVER RUN!!
 
-# Also while you're here, could you please create a file in the base directory 
-# called something.py and fill it with a hello world program?
